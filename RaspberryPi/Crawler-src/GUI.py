8355d362ca328f89f7fb0ecdb270770c2428cb14
phand <phand@purdue.edu>
2014-04-28 20:17:48 -0400

    Started Crawler GUI

---
+++ RaspberryPi/Crawler-src/GUI.py
@@ -0,0 +1,51 @@
+import wx
+
+
+class crawlerGUI(wx.Frame):
+    
+    def __init__(self, *args, **kwargs):
+        super(crawlerGUI, self).__init__(*args, **kwargs) 
+            
+        self.InitUI()
+        
+    def InitUI(self):    
+        menubar = wx.MenuBar()
+        fileMenu = wx.Menu()
+
+        dirItem = fileMenu.Append(ID_FILE_OPEN, 'Open')
+        aboutItem = fileMenu.Append(wx.ID_ABOUT, "&About", "Crawler GUI")
+        fitem = fileMenu.Append(wx.ID_EXIT, 'Quit', 'Quit application')
+        menubar.Append(fileMenu, '&File')
+        self.SetMenuBar(menubar)
+        
+        self.Bind(wx.EVT_MENU, self.OnQuit, fitem)
+        self.Bind(wx.EVT_MENU,self.onAbout,aboutItem)
+
+        self.SetSize((300, 200))
+        self.SetTitle('Crawler 0.1 BETA')
+        self.Centre()
+        self.Show(True)
+        
+    def OnQuit(self, e):
+        self.Close()
+
+    def onAbout(self,event):
+        dlg = wx.MessageDialog(self, "Crawler GUI", "Cralwer .1 Beta",wx.OK)
+        dlg.ShowModal()
+        dlg.Destroy() #KILL IT
+
+    def onButton(self,e):
+        dlg = wx.DirDialong(self, "Choose Music Directory:")
+        if dlg.ShowModel() == wx.ID_OK:
+           print "You chose this path" + dlg.GetPath()
+           dlg.Destory()
+
+def main():
+    
+    ex = wx.App()
+    crawlerGUI(None)
+    ex.MainLoop()    
+
+
+if __name__ == '__main__':
+    main()
